14ff3e1c5c81f0c3183ed8ef0e8ef0ad8e10556b
jhonmendex <jhonmendex@gmail.com>
2019-10-04 19:54:33 -0500

    Updates

---
+++ precache-manifest.bfbe1238d1475b855ecc2bd6962906ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c317f192bebb608050e31069ad1ef4",
+    "url": "/compgraf/index.html"
+  },
+  {
+    "revision": "ead2d4459aa41653d497",
+    "url": "/compgraf/static/css/main.582f3c01.chunk.css"
+  },
+  {
+    "revision": "6ca73e97964841e423dd",
+    "url": "/compgraf/static/js/2.82cf2c0e.chunk.js"
+  },
+  {
+    "revision": "ead2d4459aa41653d497",
+    "url": "/compgraf/static/js/main.3fd4851f.chunk.js"
+  },
+  {
+    "revision": "21f80df40e31a11d0039",
+    "url": "/compgraf/static/js/runtime-main.1b436f47.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/compgraf/static/media/logo.25bf045c.svg"
+  }
+]);
